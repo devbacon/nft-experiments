02d209fb4a31098f2d07b0a0f8bc9a37fd287a10
devbacon <devon.bacon@gmail.com>
2022-02-08 17:27:52 -0500

    set up store and retrieve methods for favoriteNumber

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,4 +4,13 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
 
+    uint256 favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
 }
